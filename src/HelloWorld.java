7b94377713df83570f3e080f1710857ac9f2e8ab
Irfa Sheikh <irfasheikh@gmail.com>
2021-04-23 13:44:47 -0500

    exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,95 @@
+import javax.sound.midi.Soundbank;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("Hello, ");
+        System.out.println("World!");
+
+    int myFavoriteNumber = 13;
+    System.out.println(myFavoriteNumber);
+
+
+//String myString = "I will be a programmer";
+  //  System.out.println(myString);
+
+
+//String myString = "love";
+
+        //String myString = 3.14159;
+//System.out.println(myString);
+
+
+//long myNumber = ;
+//System.out.println(myNumber);
+
+ //       long myNumber = 3.14;
+  //      System.out.println(myNumber);
+
+  //      long myNumber =123L ;
+  //      System.out.println(myNumber);
+
+  //      long myNumber = 123 ; // why does this work?
+   //     System.out.println(myNumber);
+
+      //  float myNumber = 3.14;
+      //  System.out.println(myNumber);
+//how do we fix this error? Add capital F or parenthesis float
+
+       // float myNumber = 3.14F;
+        //System.out.println(myNumber);
+       // float myNumber =
+       // myNumber= (float) 3.14;
+       // System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++); //adding after it spits out 5
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y); //adding before it spits out 5
+//        System.out.println(y);
+
+//class = lol; // doesn't work, class is a reserved word
+
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;
+// System.out.println(three); string
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+// System.out.println(three);
+ //       int three = (int) "three";
+
+
+//int x = 4;
+//x += 5;
+//        System.out.println(x);
+
+//int x = 3;
+//int y = 4;
+// y *= 4;
+//        System.out.println(x);
+//System.out.println(y);
+
+// int x = 10;
+// int y= 2;
+//   x /= y;
+// System.out.println(x);
+// System.out.println(y);
+
+//int blah = -3,147,483,648;
+
+//byte lol = 130;
+
+
+
+
+
+
+
 
     }
+
 }
