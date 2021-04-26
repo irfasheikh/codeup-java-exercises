6e339b5ae798cac29b92333e15677a044ceddb4b
Irfa Sheikh <irfasheikh@gmail.com>
2021-04-26 10:07:44 -0500

    console exercise progress

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+
+        //1.
+        double pi = 3.14159;
+System.out.println("The value of pi is approximately " + pi);
+
+        //Scanner Class// takes in user input
+
+        //1.
+        Scanner sc = new Scanner(System.in); // initializing with declaring a variable
+        System.out.println("Enter your favorite number");
+        int faveNumber = sc.nextInt();
+
+        //2.
+        System.out.println("Enter three words");
+
+        String wordOne = sc.next();
+        String wordTwo = sc.next();
+        String wordThree = sc.next();
+
+        System.out.println(wordOne + " " + wordTwo + " "+ wordThree);
+
+        //3.
+        System.out.println("Enter a sentence");
+        String aSentence = sc.next();
+
+        //4.
+        System.out.println("Type anotha one");
+        String anotherSentence = sc.nextLine();
+        // this method doesn't allow me to type in the sentence
+
+
+        //Calculate the perimeter and area of Codeup
+
+        //1.
+        System.out.println("Enter the length and width of classrom");
+
+
+
+
+    }
+}
+
--- src/ConsoleLecture.java
+++ src/ConsoleLecture.java
@@ -1,50 +1,71 @@
-public class ConsoleIOLecture {
+import java.util.Scanner;
+
+public class ConsoleLecture {
     public static void main(String[] args) {
         System.out.println("Console IO Lecture");
 
-        // ============================== print() VS println()
+        // ================================== print() and println()
 
         // this:
-        System.out.println("here");
-        System.out.println("there");
+//        System.out.println("here");
+//        System.out.println("there");
 
         // is equivalent to this:
-        System.out.print("here\n");
-        System.out.println("there\n");
+//        System.out.print("here\n");
+//        System.out.print("there\n");
 
         // without the newline characters, print outputs this:
-        System.out.print("here");
-        System.out.println("there");
+//        System.out.print("here");
+//        System.out.print("there");
+
 
         // to concatenate, just like JS:
         String firstName = "Douglas";
         String lastName = "Hirsh";
-        System.out.println(firstName + " " + lastName);
+//        System.out.println(firstName + " " + lastName);
 
-        // ============================== printf() / .format()
+        // ================================== printf() / .format()
 
-        // Print a formatted string using the following... printf(formatString)
+        // Print a formatted string using the following... printf(formatString, data)
 
         // same output as print():
-        System.out.printf("Hello");
-        System.out.printf("There");
+//        System.out.printf("Hello");
+//        System.out.printf("There");
 
         // multiple variables:
         System.out.printf("Hello, %s!\n", firstName);
         System.out.printf("Hello, %s %s!\n", firstName, lastName);
 
-        // numbers:
-        int numberOfPets =  3;
-        String typeOfPets =  "cats";
-        System.out.printf("I have %d %s.\n", numberOfPets, typeOfPets);
+        // numbers
+        int numberOfPets = 3;
+        String typeOfPets = "cats";
+        System.out.printf("I have %d %s.\n",numberOfPets, typeOfPets);
 
         // currency:
         float currencyPennies = 1000.001F;
-        System.out.printf("My iPhone cost $%.3f\n", currencyPennies);
-        System.out.printf("My iPhone cost $%.2f", currencyPennies);
+        System.out.printf("My iPhone cost $%.2f\n", currencyPennies);
 
+        // ================================== USER INPUT
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your first and last name?");
+        String userFirstName = sc.next();
+        String userLastName = sc.next();
 
+        System.out.printf("Nice to meet you, %s %s\n", userFirstName, userLastName);
 
+        System.out.println("How old are you?");
+
+        int age = sc.nextInt();
+
+        System.out.printf("Good to hear you are %d years old.\n", age);
+
+        // Quirk of using next() then nextLine()...
+        // https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
+        sc.nextLine();
+        System.out.println("Favorite Quote:");
+
+        String favoriteQuote = sc.nextLine();
+        System.out.printf("Funny, \"%s\" is my favorite quote too.", favoriteQuote);
 
 
     }
