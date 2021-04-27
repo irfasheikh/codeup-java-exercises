c48ab94a1548033c88edf661d3fa6c915ed1e9aa
Irfa Sheikh <irfasheikh@gmail.com>
2021-04-27 10:32:26 -0500

    finished exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -88,28 +88,51 @@ public class ControlFlowExercises {
         //3. Display a table of powers
 
         //prompt the user
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to?");
+//        int power = scanner.nextInt();
+//
+//        // print the table
+//        System.out.println("Here is your table!");
+//        System.out.println("number | squared | cubed");
+//        System.out.println("-------|---------|-----");
+//
+//        for ( int i = 1; i <= power; i++){
+//            int squared = i * i;
+//            int cubed = i * i * i;
+//            System.out.printf("%d | %d | %d \n", i, squared, cubed);
+//        }
+
+
+        //4. Grades
+
+        // prompt
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What number would you like to go up to?");
-        int power = scanner.nextInt();
+        boolean confirmation; // have to identify the boolean value, this is what will confirm what the user inputs 'this boolean is called confirmation'
+        do {
+            System.out.println("Enter your grade");
+            int grade = scanner.nextInt();
+            if (grade >= 88) {
+                System.out.println("You earned an A!");
+            } else if (grade >= 80) {
+                System.out.println("You earned a B!");
+            } else if (grade >= 67) {
+                System.out.println("You earned a C!");
+            } else if (grade >= 60) {
+                System.out.println("You got an F");
+            }
+            System.out.println("Would you like to continue? [y/n]");
+            String userInput = scanner.next();
+            confirmation = userInput.equalsIgnoreCase("y");
+        } while (confirmation);
 
-        // print the table
-        System.out.println("Here is your table!");
-        System.out.println("number | squared | cubed");
-        System.out.println("-------|---------|-----");
 
-        for ( int i = 1; i <= power; i++){
-            int squared = i * i;
-            int cubed = i * i * i;
-            System.out.printf("%d | %d | %d \n", i, squared, cubed);
-        }
-
-
-
-
+    }
 
 
             }
-        }
+
 
 
 
