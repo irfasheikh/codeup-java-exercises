cd23f1a28a93ec4bdcc773fe8e3f5b469d6695a1
Irfa Sheikh <irfasheikh@gmail.com>
2021-04-27 16:18:31 -0500

    end of day exercise progress

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -7,6 +7,7 @@ public class MethodsExercises {
         System.out.println(multiplication(1, 2));
         System.out.println(division(1, 2));
         System.out.println(modulus(1, 2));
+        getInteger(1,10);
     }
 
     // Basic Arithmetic
@@ -33,32 +34,28 @@ public class MethodsExercises {
     }
 
 
-//2. Range //kenneth said dont use recursion
-public static int getInteger(int min, int max){
+//2. Range //
+
+public static void getInteger(int min, int max){
     Scanner scanner = new Scanner(System.in);
 
+        boolean wrongInput = false;
         do {
-            System.out.print("Enter a number between 1 and 10: ");
-            int userInput = getInteger(1, 10);
+            System.out.printf("Enter a number between %d and %d",min, max);
+            int userInput = Integer.parseInt(scanner.next());
 
-            if( userInput >= 1) {
-                System.out.print("This is in the range");
-            }
-            else if (userInput <= 10) {
-                System.out.print("We are in the range");
+            if( userInput >= min && userInput <= max) {
+                System.out.println("We are in the range");
 
-            } else if()
+            } else if (wrongInput== true) {
+            System.out.println("Enter a valid number");}
 
 
-                } while (true);
+                } while (wrongInput); //if they put the wrong thing, they're trapped in the loop, to get the loop to stop, we have to make it false
             }
 
 
     }
 
-
-
-
-
-
-}
+//curly braces are the instructions for whats supposed to happen inside
+//put the system inside of {}
