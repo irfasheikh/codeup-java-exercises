e313f58f81f7829dc79a4ec1e305e9dfabfe0265
Irfa Sheikh <irfasheikh@gmail.com>
2021-05-03 09:37:59 -0500

    exercise

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,27 @@
 public class Person {
-    private class Person{
 
+
+    private String namePerson;
+
+
+
+    //constructor
+
+
+
+    public String getName(){
+//TODO: return the person's name
+        return namePerson;
     }
+
+    public String setName(String name){
+//TODO: change the name property to the passed value
+        return namePerson = name;
+    }
+    public String sayHello() {
+//TODO: print a message to the console using the person's name
+        return "Hey wassup hello " + namePerson;
+    }
+
 }
+
